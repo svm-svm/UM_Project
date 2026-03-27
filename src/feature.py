794d2fa3d23a5c558fe1d8cde183f68019648fa0
svm-svm <120033865+svm-svm@users.noreply.github.com>
2026-03-27 18:50:03 +0530

    Update feature.py

--- src/feature.py
+++ src/feature.py
@@ -3,14 +3,23 @@ import numpy as np
 import joblib
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("edupro_prepared_dataset.csv")
-model = joblib.load("revenue_model.pkl")
+data = pd.read_csv("processed_edupro_data.csv")
+
+loaded = joblib.load("model/revenue_model.pkl")
+model = loaded["model"]
 
 preprocessor = model.named_steps["prep"]
 estimator = model.named_steps["model"]
-
-# feature names
-X = data.drop(columns=["CourseID","CourseName","EnrollmentCount","CourseRevenue"], errors="ignore")
+#feature names
+X = data.drop(columns=[
+    "CourseID",
+    "CourseName",
+    "EnrollmentCount",
+    "CourseRevenue",
+    "CategoryRevenue",
+    "MonthlyEnrollments",
+    "AvgRevenuePerEnrollment"
+], errors="ignore")
 
 num_cols = X.select_dtypes(include=np.number).columns.tolist()
 cat_cols = X.select_dtypes(include="object").columns.tolist()
@@ -20,13 +29,10 @@ cat_names = ohe.get_feature_names_out(cat_cols)
 
 feature_names = np.concatenate([num_cols, cat_names])
 
-
 if hasattr(estimator, "feature_importances_"):
     importances = estimator.feature_importances_
-
 elif hasattr(estimator, "coef_"):
-    importances = np.abs(estimator.coef_)   
-
+    importances = np.abs(estimator.coef_)
 else:
     raise ValueError("Model does not support importance extraction")
 
@@ -36,7 +42,6 @@ imp_df = pd.DataFrame({
 }).sort_values("Importance", ascending=False)
 
 print(imp_df.head(20))
-
 
 plt.figure(figsize=(10,6))
 plt.barh(imp_df.head(15)["Feature"][::-1], imp_df.head(15)["Importance"][::-1])
